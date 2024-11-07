f742a6b5df279c6f7e23463e7790267f6adc3cf8
Bohdan Havran <danohavran@gmail.com>
2024-11-07 22:07:14 +0200

    feature: modify django.groovy

--- django.groovy
+++ django.groovy
@@ -1,47 +1,44 @@
 pipeline {
-    agent { label 'worker' }
+  agent { label 'worker' }
 
-    environment {
-        IMAGE_NAME = "django-docker:1.1"
-        TOKEN = credentials("botSecret")
-        CHAT_ID = credentials("chatId")
-    }
+  environment {
+    IMAGE_NAME = "django-docker"
+    TOKEN = credentials("botSecret")
+    CHAT_ID = credentials("chatId")
+  }
 
-    stages {
-        stage("Verify Docker Access") {
-            steps {
-                sh "docker ps"
-            }
+  stages {
+    stage('Version Calculation') {
+      steps {
+        script {
+          env.IMAGE_VERSION = sh(script: "git rev-parse --short=6 HEAD", returnStdout: true).trim()
         }
-        stage("Build Docker Image") {
-            steps {
-                script {
-                    def dockerImage = docker.build("${IMAGE_NAME}", ".")
-                }
-            }
-        }
-        stage("Test Docker Image") {
-            steps {
-                sh "whoami"
-            }
-        }
+      }
     }
+    stage("Build Docker Image") {
+      steps {
+        script {
+          dockerImage = docker.build("${IMAGE_NAME}:${IMAGE_VERSION}")
+        }
+      }
+    }
+  }
 
-    post {
-        success { 
-            sh ("""
-                curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [OK](${BUILD_URL}consoleFull)'
-            """)
-        }
-        aborted {
-            sh ("""
-                curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [Aborted](${BUILD_URL}consoleFull)'
-            """)
-        }
-        failure {
-            sh ("""
-                curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [Not OK](${BUILD_URL}consoleFull)'
-            """)
-        }
+  post {
+    success { 
+      sh ("""
+        curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [OK](${BUILD_URL}consoleFull)'
+      """)
     }
+    aborted {
+      sh ("""
+        curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [Aborted](${BUILD_URL}consoleFull)'
+      """)
+    }
+    failure {
+      sh ("""
+        curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [Not OK](${BUILD_URL}consoleFull)'
+      """)
+    }
+  }
 }
