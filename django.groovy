b6d4e57bb503cd4a9b6e928cfa43917c0471d618
Bohdan Havran <danohavran@gmail.com>
2024-11-07 03:29:26 +0200

    feature: modify django.groovy

--- django.groovy
+++ django.groovy
@@ -3,6 +3,8 @@ pipeline {
 
   environment {
     IMAGE_NAME = "django-docker:1.1"
+    TOKEN = credentials("botSecret")
+    CHAT_ID = credentials("chatId")
   }
 
   stages {
@@ -21,8 +23,22 @@ pipeline {
   }
 
   post {
-    always {
-      echo "Pipeline is done"
+        success { 
+            sh  ("""
+                curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [OK](${BUILD_URL}consoleFull)'
+            """)
+        }
+
+        aborted {
+            sh  ("""
+                curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [Aborted](${BUILD_URL}consoleFull)'
+            """)
+        }
+        failure {
+            sh  ("""
+                curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode=markdown -d text='*Full project name*: ${env.JOB_NAME} \n*Branch*: [$GIT_BRANCH]($GIT_URL) \n*Build* : [Not OK](${BUILD_URL}consoleFull)'
+            """)
+        }
+
     }
-  }
 }
