515fa1080209cc1b82760d1c396e51408f6a046d
Bohdan Havran <danohavran@gmail.com>
2024-11-07 23:33:40 +0200

    feature: modify django.groovy

--- django.groovy
+++ django.groovy
@@ -33,7 +33,7 @@ pipeline {
     stage("Push Docker Image") {
       steps {
         script {
-          withDockerRegistry(credentialsId: 'DockerHub', url: 'https://index.docker.io/v1/') {
+          docker.withRegistry('https://index.docker.io/v1/', 'DockerHub') {
             dockerImage.push("${IMAGE_VERSION}")
           }
         }
