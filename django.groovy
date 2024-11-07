16178ebd8e3bab3289414c8c91a5fcfd1db3ac83
Bohdan Havran <danohavran@gmail.com>
2024-11-07 02:20:16 +0200

    feature: modify django.groovy

--- django.groovy
+++ django.groovy
@@ -1,10 +1,21 @@
 pipeline {
   agent any
 
+  environment {
+    IMAGE_NAME = "django-docker:1.1"
+  }
+
   stages {
-    stage("Hello") {
+    stage("Build Docker Image") {
       steps {
-        echo "hello"
+        script {
+            docker.build(IMAGE_NAME)
+        }
+      }
+    }
+    stage("Test Docker Image") {
+      steps {
+        echo "Test"
       }
     }
   }
