e914f00278af0ed2be321b6163fa608d158ad0d6
Bohdan Havran <danohavran@gmail.com>
2024-11-08 15:41:40 +0200

    feature: modify terraform

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,13 +2,3 @@ variable "do_token" {
   description = "DigitalOcean Token"
   type        = string
 }
-
-# variable "spaces_access_key_id" {
-#   description = "DigitalOcean Spaces Access Key ID"
-#   type        = string
-# }
-
-# variable "spaces_secret_access_key" {
-#   description = "DigitalOcean Spaces Secret Access Key"
-#   type        = string
-# }
